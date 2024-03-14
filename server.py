d73f3fe3362e8af3113619992397a6657d3fd466
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2024-03-13 23:01:05 -0300

    Fix: server

--- server.py
+++ server.py
@@ -1,12 +1,8 @@
 import asyncio
 import websockets
-import threading
 
 class Server():
     
-    def __init__(self):
-        pass
-
     async def ping_pong(self, websocket, path):
         async for mensagem in websocket:
             if mensagem == "ping":
@@ -15,17 +11,11 @@ class Server():
                 await websocket.send("pong")
                 print("Enviado: pong")
 
-    def start_server(self):
-        # Cria um novo loop de eventos para o thread
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        
+    async def start_server(self):
         # Inicia o servidor dentro do loop de eventos
-        start_server = websockets.serve(self.ping_pong, "localhost", 8765)
-        loop.run_until_complete(start_server)
-        loop.run_forever()
+        async with websockets.serve(self.ping_pong, "localhost", 8765):
+            await asyncio.Future()  # Mantém o servidor em execução para sempre
 
 if __name__ == "__main__":
     server = Server()
-    server_thread = threading.Thread(target=server.start_server)
-    server_thread.start()
+    asyncio.run(server.start_server())
